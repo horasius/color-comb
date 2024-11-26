64ad36ee4931db4a1fe324d64bd04e33cb22f57a
horasius <i.m.burhan@gmail.com>
2024-11-26 14:17:04 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,6 @@
-window.addEventListener('DOMContentLoaded', () => {
-  const header = document.querySelector('header');
-  const main = document.querySelector('main');
-  const footer = document.querySelector('footer');
-
-  header.textContent = 'Сolor combinations';
-
-  const button = document.createElement('button');
-  button.textContent = 'Wanna colors?';
-  main.append(button);
-
-  let changeColors = function() {
-    let newColor = '#' + Math.random().toString(16).slice(3, 9);
-    let newColorReversed = '#' + (Math.random().toString(16).slice(3, 9)).split().reverse().join();
-    let bodyHexName = `This color is ${newColorReversed}`;
-    let btnHexName = `This color is ${newColor}`;
-
-    document.body.style.backgroundColor = newColorReversed;
-    button.style.backgroundColor = newColor;
-    button.textContent = btnHexName;
-    footer.textContent = bodyHexName;
-  };
-
-  button.addEventListener('click', changeColors);
-})
+// script.js
+// Add interactive elements like:
+// - Modals for displaying more information
+// - Animations
+// - Form validation
+// - AJAX requests for dynamic content
